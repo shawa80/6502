1d0d3df50d343216c034e22bf708d2acf62a88b3
andrew <andrew@localhost.localdomain>
2021-06-14 07:45:34 -0500

    init check in

---
+++ hello-world.s
@@ -0,0 +1,164 @@
+_dis_portB = $6000
+_dis_portA = $6001
+_dis_DDRB = $6002
+_dis_DDRA = $6003
+
+_timer1_T1C_L = $6004
+_timer1_T1C_H = $6005
+_timer1_T1L_L = $6006
+_timer1_T1L_H = $6007
+_timer1_T2C_L = $6008
+_timer1_T2C_H = $6009
+
+_SR = $600A
+_ACR = $600B
+_PCR = $600C
+_IFR = $600D
+_IER = $600E
+_ORA = $600F
+
+
+PORTB = $6000
+PORTA = $6001
+DDRB = $6002
+DDRA = $6003
+T1C_L = $6004
+T1C_H = $6005
+T1L_L = $6006
+T1L_H = $6007
+T2C_L = $6008
+T2C_H = $6009
+SR = $600A
+ACR = $600B
+PCR = $600C
+IFR = $600D
+IER = $600E
+ORA = $600F
+
+; A, X, Z are not preserved on calls
+
+REG_B  = $02  ; general purpose regs
+REG_BL = $02
+REG_BH = $03
+
+REG_C  = $04
+REG_CL = $04
+REG_CH = $05
+
+REG_D  = $06
+REG_DL = $06
+REG_DH = $07
+
+REG_E  = $08
+REG_EL = $08
+REG_EH = $09
+
+REG_R  = $0A  ; reg for return values
+REG_RL = $0A
+REG_RH = $0B
+
+r0  = $00
+r1  = $01
+r2  = $02
+r3  = $03
+r4  = $04
+r5  = $05
+r6  = $06
+r7  = $07
+r8  = $08
+r9  = $09
+r10 = $0A
+r11 = $0B
+r12 = $0C
+r13 = $0D
+r14 = $0E
+r15 = $0F
+r16 = $10
+r17 = $11
+r18 = $12
+r19 = $13
+r20 = $14
+r21 = $15
+r22 = $16
+r23 = $17
+r24 = $18
+r25 = $19
+r26 = $1A
+r27 = $1B
+r28 = $1C
+r29 = $1D
+r30 = $1E
+r31 = $1F
+
+btmp0 = $20  ;20 - 23
+btmp1 = $24  ;24 - 27
+btmp2 = $28  ;28 - 2b
+btmp3 = $2c  ;2c - 2f
+
+sp  = $30
+sp0 = $30
+sp1 = $31
+
+;$0032 - $00ff   ; open zero page
+;$0100 - $01ff ; hardware stack
+
+cStackL = $0200
+cStackH = $0600
+
+_vtest = $33
+
+
+_clk_hr = $34
+_clk_min = $35
+_clk_sec = $36
+_clk_jiff = $37 
+
+_com_index = $38
+_com_size = $39
+_com_buf = $40
+_com_buf_end = $A4
+
+
+  .org $8000
+
+reset:
+  ldx #$ff
+  txs
+
+
+  ; init c stack
+  lda #00
+  sta sp0
+  lda #06
+  sta sp1
+
+main:
+
+  jsr _start
+
+
+
+loop:
+  jmp loop
+
+_sys_irqEnable:
+  cli
+  rts
+
+
+  .include test.asm
+
+
+nmi:
+irq:
+;  pha
+;  bit T1C_L
+;  jsr _clk_tick
+;  pla
+  jsr _irqHandler
+  rti
+
+  .org $fffa
+  .word nmi
+  .word reset
+  .word irq
---
+++ test.c
@@ -0,0 +1,400 @@
+
+extern char vtest;
+
+extern char dis_portA;
+extern char dis_portB;
+extern char dis_DDRB;
+extern char dis_DDRA;
+
+extern char timer1_T1C_L;
+extern char timer1_T1C_H;
+extern char timer1_T1L_L;
+extern char timer1_T1L_H;
+
+extern char timer1_T2C_L;
+extern char timer1_T2C_H;
+
+extern char SR;
+extern char ACR;
+extern char PCR;
+extern char IFR;
+extern char IER;
+extern char ORA;
+
+#define dis_E 0x80
+#define dis_RW 0x40
+#define dis_RS 0x20
+
+
+#define VIA_IRQ    ((char)0x80)
+#define VIA_TIMER1 ((char)0x40)
+#define VIA_TIMER2 ((char)0x20)
+#define VIA_CB1    ((char)0x10)
+#define VIA_CB2    ((char)0x80)
+#define VIA_SHIFT  ((char)0x04)
+#define VIA_CA1    ((char)0x02)
+#define VIA_CA2    ((char)0x01)
+
+
+void sys_irqEnable();
+
+extern char com_index;
+extern char com_size;
+extern char com_buf[];
+
+void com_init() {
+
+	char t = ACR;
+	t &= 0xE3;	  //111x xx11 clear bits
+//	t |= 0x18; //xxx1 10xx //set bits
+	t |= 0x14; //xxx1 10xx //set bits
+
+	ACR = t;
+
+	com_index = 0;
+	com_size = 28;
+	
+	//start frame
+	com_buf[0] = 0x00;  //end frame?
+	com_buf[1] = 0x00;
+	com_buf[2] = 0x00;
+	com_buf[3] = 0x00;
+
+	//start frame
+	com_buf[4] = 0x00;
+	com_buf[5] = 0x00;
+	com_buf[6] = 0x00;
+	com_buf[7] = 0x00;
+
+
+		//blue
+		com_buf[8] = 0xE1; //global
+		com_buf[9] = 0xFF; //b
+		com_buf[10] = 0x00; //g
+		com_buf[11] = 0x00; //r
+
+		//red
+		com_buf[12] = 0xE4; //global
+		com_buf[13] = 0x00; //b
+		com_buf[14] = 0x00; //g
+		com_buf[15] = 0xFF; //r
+
+		//green
+		com_buf[16] = 0xE4; //global
+		com_buf[17] = 0x00; //b
+		com_buf[18] = 0xFF; //g
+		com_buf[19] = 0x00; //r
+
+		//green
+		com_buf[20] = 0xFF; //global
+		com_buf[21] = 0xFF; //b
+		com_buf[22] = 0xFF; //g
+		com_buf[23] = 0xFF; //r
+
+		//green
+		com_buf[24] = 0xFF; //global
+		com_buf[25] = 0xFF; //b
+		com_buf[26] = 0xFF; //g
+		com_buf[27] = 0xff; //r
+
+}
+
+inline void com_write(char d) {
+
+	SR = d;
+}
+
+inline char com_read() {
+	return SR;
+}
+
+inline void com_enableIrq() {
+	IER = (VIA_IRQ | VIA_SHIFT); //0x84;
+}
+inline void com_disableIrq() {
+	IER = (VIA_SHIFT); //0x04;
+}
+
+inline void com_procIrq() {
+
+
+	if (com_index < com_size)
+	{
+		com_index++;
+		com_write(com_buf[com_index]);
+	}
+	else {
+		com_disableIrq();
+	}
+}
+
+void com_start() {
+	com_procIrq();
+}
+
+
+void dis_wait() {
+	dis_DDRB = 0x00;
+	while (1) {
+		dis_portA = dis_RW;
+		dis_portA = (dis_RW|dis_E);
+		if ((dis_portB & 0x80) != 0x80) break;
+	}
+	dis_portA = dis_RW;
+	dis_DDRB = 0xff; 
+}
+
+void dis_writePortA(char val) {
+	dis_portA = val;
+}
+void dis_writePortB(char val) {
+	dis_portB = val;
+}
+
+void dis_issueIns(char instruction) {
+	dis_wait();
+	dis_writePortB(instruction);
+	dis_writePortA(0);
+	dis_writePortA(0x80);
+	dis_writePortA(0);
+}
+
+void dis_clear() {
+	dis_issueIns(1);
+}
+
+void dis_home() {
+	dis_issueIns(2);
+}
+
+void dis_setLine2() {
+	dis_issueIns(0xC0);
+}
+
+void dis_printChar(char c) {
+	dis_wait();
+	dis_portB = c;
+	dis_portA = dis_RS;
+	dis_portA = (dis_RS | dis_E);
+	dis_portA = dis_RS;
+}
+
+
+int math_div(int num, int deno) {
+
+	int times = 0;
+	while (num >= deno) {
+		num = num - deno;
+		times++;
+	}
+	return times;
+}
+
+int math_mod(int num, int deno) {
+
+	while (num >= deno) {
+		num = num - deno;
+	}
+	return num;
+}
+
+char dis_int2Ascii(int value) {
+	return 0x30 + value;
+}
+
+void dis_printDec(int value) {
+
+	if (value < 10) {
+		dis_printChar(dis_int2Ascii(value));
+		return;
+	}
+
+	int remainder = math_mod(value, 10);
+	value = math_div(value, 10);
+
+	dis_printDec(value);
+	dis_printChar(dis_int2Ascii(remainder));
+}
+
+
+void dis_printInit() {
+	dis_DDRB = 0xFF; //set all pins to outpyut
+	dis_DDRA = 0xE0; //set top 3 pins on A to output
+
+	dis_issueIns(0x38); //set 8-bit mode; 2 line display, 5x8 font
+	dis_issueIns(0x0C); //set display on; cursor off; blink off
+	dis_issueIns(0x06); //increment and shift cursor
+
+	dis_clear();
+}
+
+void dis_print(char * c) {
+
+	int i = 0;
+	while (c[i] != '\0') {
+		dis_printChar(c[i]);
+		i++;
+	}
+}
+
+void mem_cpy(char dest[], char src[], int size) {
+
+	for (int i = 0; i < size; i++) {
+		dest[i] = src[i];
+	}
+}
+
+
+void mem_set(short addr[], int size, char value) {
+
+	for (int i = 0; i < size; i++) {
+		addr[i] = value;
+	}
+}
+
+int mem_scanMem(char * startAddress) {
+	int i = 0;
+	while (1) {
+		startAddress[i] = (char)0xA5; //set value here
+		if (startAddress[i] != (char)0xA5) {
+			break;
+		}
+		i++;
+	}
+	return i+(int)startAddress;
+}
+
+
+void timer1_init() {
+
+	timer1_T1C_L = 0x10;
+	timer1_T1C_H = 0x27;
+
+	timer1_T1L_L = 0x10;
+	timer1_T1L_H = 0x27;
+
+//ex
+	timer1_T2C_L = 0x10;
+	timer1_T2C_H = 0x27;
+
+
+	ACR = 0x40;
+}
+
+void timer1_start() {
+	IER = 0xC0;
+}
+
+void timer1_clearIrq() {
+	char tmp;
+	tmp = timer1_T1C_L;
+}
+
+extern char clk_jiff;
+extern char clk_sec;
+extern char clk_min;
+extern char clk_hr;
+
+void clk_init() {
+	clk_jiff = 0;
+	clk_sec = 0;
+	clk_min = 0;
+	clk_hr = 1;
+}
+
+void clk_print() {
+
+	dis_clear();
+	dis_home();
+	dis_setLine2();
+
+	dis_printDec(clk_hr);
+	dis_print(":");
+	if (clk_min < 10)
+		dis_print("0");
+	dis_printDec(clk_min);
+	dis_print(":");
+	if (clk_sec < 10)
+		dis_print("0");
+	dis_printDec(clk_sec);
+}
+
+void clk_tick() {
+	int print = 0;
+
+	clk_jiff++;
+	if (clk_jiff >= 100)
+	{
+		clk_jiff = 0;
+		clk_sec++;
+		print = 1;
+	}
+	if (clk_sec >= 60)
+	{
+		clk_sec = 0;
+		clk_min++;
+	}
+	if (clk_min >= 60)
+	{
+		clk_min = 0;
+		clk_hr++;
+	}
+	if (clk_hr >= 13)
+	{
+		clk_hr = 1;
+	}
+
+	if (print == 1) {
+		clk_print();
+		//com_write(0xA5);
+	}
+}
+
+
+
+
+inline void irqHandler() {
+
+	char via = IFR;
+
+	if (via & VIA_TIMER1) {
+		timer1_clearIrq();
+		clk_tick();
+	}
+	if (via & VIA_SHIFT) {
+		com_procIrq();
+	}
+
+}
+
+
+void start() {
+
+	vtest = 1;
+	clk_init();
+	dis_printInit();
+	timer1_init();
+
+	com_init();
+	com_enableIrq();
+
+	dis_clear();
+	dis_home();
+	dis_print("Hello From C");
+
+	dis_setLine2();
+	dis_print("Memory: ");
+	int memSize = mem_scanMem((char*)2048);
+	dis_printDec(memSize);
+
+	timer1_start();
+
+	sys_irqEnable();
+
+	com_start();
+
+	while (1){}
+}
+
+
+
